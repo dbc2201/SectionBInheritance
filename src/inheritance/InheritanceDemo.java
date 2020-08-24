9c01d9b6bf367f6f42ec0d265ddfca00065dace1
dbc2201 <divyanshb22@gmail.com>
2020-08-24 13:43:48 +0530

    add the uses of Inheritance

--- src/inheritance/InheritanceDemo.java
+++ src/inheritance/InheritanceDemo.java
@@ -26,7 +26,24 @@ public class InheritanceDemo {
     // Inheritance is the mechanism by which a class can acquire
     // the properties(fields/variables) and/or methods of another class related to it.
 
-    // 2. How we can use Inheritance to re-use code/programs/software that are already developed are tested.
+    // 2. How we can use Inheritance to re-use code/programs/software that are already developed and tested.
+
+    // Re-Use of a code/program/software :
+    // It means that if some developer(s) have created a code/program/software,
+    // we can modify the code/program/software without having to start from scratch.
+
+    // 1. use this software as an end-user:
+    // we will simply download the final product, install it, and use it.
+    // An end-user might not be an engineer, or a person that knows how to write code.
+
+    // 2. use this software as a developer:
+    // we can observe/modify the source code of the software according to our liking.
+
+    // The types of software that exist in the world:
+    // 1. Open Source Software: The Java JDK is also a part of Open Source.
+    //  i. will allow you to observe and/or modify the source code.
+    // 2. Closed Source Software, ex - Microsoft Windows, Microsoft Office
+
     // 3. Observe how to use the `extends` keyword.
     // 4. What is a `super` class and what is a `sub` class.
     // 5. How we can access the members of a super class from a sub class.
