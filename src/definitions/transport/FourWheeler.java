26aebcee9daef12d148dc7a7380a4ccfc407312e
dbc2201 <divyanshb22@gmail.com>
2020-08-26 13:14:41 +0530

    create Vehicle.java and FourWheeler.java

---
+++ src/definitions/transport/FourWheeler.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 26/08/20
+ *  Time: 1:14 PM
+ *  File Name : FourWheeler.java
+ * */
+package definitions.transport;
+
+public class FourWheeler {
+}
---
+++ src/definitions/transport/Vehicle.java
@@ -0,0 +1,11 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 26/08/20
+ *  Time: 1:13 PM
+ *  File Name : Vehicle.java
+ * */
+package definitions.transport;
+
+public class Vehicle {
+
+}
