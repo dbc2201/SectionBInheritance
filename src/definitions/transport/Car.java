7b4872c03c970aa7b92b513e31af3373c8c0a520
dbc2201 <divyanshb22@gmail.com>
2020-08-26 13:30:47 +0530

    add example of hierarchical inheritance

--- src/definitions/transport/Car.java
+++ src/definitions/transport/Car.java
@@ -6,9 +6,19 @@
  * */
 package definitions.transport;
 
-public class Car extends Vehicle {
+public class Car extends FourWheeler {
 
 }
+
+// Vehicle <- FourWheeler <- Car
+// The Vehicle.java class is the parent class of the FourWheeler.java class.
+// The FourWheeler.java class is the child class of the Vehicle.java class.
+// The FourWheeler.java class is also the parent class of the Car.java class.
+// The Car.java class is the child class of the FourWheeler.java class.
+// Vehicle <grandparent> <- FourWheeler <parent> <- Car <child>
+
+// A Car IS A type of Vehicle.
+// A Car IS A type of FourWheeler as well.
 
 // The Vehicle class is the parent class of the Car class.
 // The Car class is the child class of the Vehicle class.
--- src/inheritance/README.md
+++ src/inheritance/README.md
@@ -3,6 +3,6 @@
 ## Types of Inheritance
 1. Single (Simple) Inheritance.
 2. Hierarchical Inheritance.
-3. Multiple Inheritance.
+3. Multiple Inheritance.(NOT ALLOWED IN JAVA!!)
 4. Multi-level Inheritance.
 5. Hybrid Inheritance.
