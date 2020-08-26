f2793200946ed0302f32a8b1f92bc5d38dc7cc5b
dbc2201 <divyanshb22@gmail.com>
2020-08-26 13:21:45 +0530

    add example for hierarchical inheritance

--- src/definitions/transport/Car.java
+++ src/definitions/transport/Car.java
@@ -9,3 +9,15 @@ package definitions.transport;
 public class Car extends Vehicle {
 
 }
+
+// The Vehicle class is the parent class of the Car class.
+// The Car class is the child class of the Vehicle class.
+// Vehicle <- Car
+
+// The Vehicle class is also the parent of the FourWheeler class.
+// & The FourWheeler class is also a child of the Vehicle class.
+// Vehicle <- FourWheeler
+
+// Vehicle <- Car & FourWheeler
+// Vehicle <- Car
+// Vehicle <- FourWheeler
