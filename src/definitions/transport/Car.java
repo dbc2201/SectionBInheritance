b0d70061392228cf74cb85de092354bf6fae44da
dbc2201 <divyanshb22@gmail.com>
2020-08-26 13:17:41 +0530

    create Car.java

---
+++ src/definitions/transport/Car.java
@@ -0,0 +1,11 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 26/08/20
+ *  Time: 1:16 PM
+ *  File Name : Car.java
+ * */
+package definitions.transport;
+
+public class Car {
+
+}
