f4d6ff1986f86fbf0003a44fe76a87ff0dba09c4
dbc2201 <divyanshb22@gmail.com>
2020-08-26 13:43:40 +0530

    add example for method overloading in the child class

--- src/definitions/transport/FourWheeler.java
+++ src/definitions/transport/FourWheeler.java
@@ -7,7 +7,10 @@
 package definitions.transport;
 
 public class FourWheeler extends Vehicle {
-
+    public void travel(String source, String destination) {
+        System.out.println("FourWheeler is travelling from " +
+                source + " to " + destination + ".");
+    }
 }
 
 // The Vehicle.java class is the parent class of the FourWheeler.java class.
--- src/execution/Main.java
+++ src/execution/Main.java
@@ -6,11 +6,14 @@
  * */
 package execution;
 
+import definitions.transport.FourWheeler;
 import definitions.transport.Vehicle;
 
 public class Main {
     public static void main(String[] args) {
         Vehicle myVehicle = new Vehicle();
         myVehicle.travel("Agra");
+        FourWheeler myFourWheeler = new FourWheeler();
+        myFourWheeler.travel("Delhi", "Agra");
     }
 }
