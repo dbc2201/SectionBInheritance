8bc25426dad2a52238faedfe0685d0cbb30865b6
dbc2201 <divyanshb22@gmail.com>
2020-08-27 13:35:58 +0530

    add example of simple inheritance

--- src/definitions/examples/Example1.java
+++ src/definitions/examples/Example1.java
@@ -8,17 +8,20 @@ package definitions.examples;
 
 public class Example1 {
     public static void main(String[] args) {
-
+        DemoA objectA = new DemoA();
+        objectA.display();
+        DemoB objectB = new DemoB();
+        objectB.display();
     }
 }
 class DemoA { // visibility modifier: default
-    public void display() {
+    public void display() { // method 1
         System.out.println("Parent Class Method.");
     }
 }
 class DemoB extends DemoA {
     @Override
-    public void display() { // method overriding
+    public void display() { // method 2, method overriding
         System.out.println("Child Class Method.");
     }
 }
