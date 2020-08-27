0b159c80148396262d1a8b3e0ea1c0913643355d
dbc2201 <divyanshb22@gmail.com>
2020-08-27 13:59:44 +0530

    add example for calculate; partial

--- src/definitions/examples/calculator/Calculator.java
+++ src/definitions/examples/calculator/Calculator.java
@@ -7,4 +7,48 @@
 package definitions.examples.calculator;
 
 public class Calculator {
+    public static void main(String[] args) {
+
+    }
 }
+class A {
+    public int i, j;
+
+    void setValues(int x, int y) {
+        i = x;
+        j = y;
+        System.out.println("x = " + x + ", y = " + y);
+        System.out.println("i = " + i + ", j = " + j);
+    }
+
+    public int sum(int x, int y) {
+        int sum = i + j;
+        return sum;
+    }
+}
+class B extends A {
+    public int product(int x, int y) {
+        int product = i * j;
+        return product;
+    }
+}
+class C extends A {
+    public int division(int x, int y) {
+        int division = i / j;
+        return division;
+    }
+
+    public int difference(int x, int y) {
+        int difference = i - j;
+        return difference;
+    }
+}
+
+
+
+
+
+
+
+
+
