1ad180a8b0706b440d5d810ded584b59fd67be0b
dbc2201 <divyanshb22@gmail.com>
2020-08-27 13:53:24 +0530

    add examples for hierarchical and multi-level inheritance

--- src/definitions/examples/Example2.java
+++ src/definitions/examples/Example2.java
@@ -8,6 +8,41 @@ package definitions.examples;
 
 public class Example2 {
     public static void main(String[] args) {
+        D objectD = new D();
+        objectD.methodA(4); // A <- B <- D
+        objectD.methodD(5);
+    }
+}
+class A {
+    int j = 0;
 
+    public void methodA(int k) {
+        j = k;
+        System.out.println("Class A : j = " + j);
+    }
+}
+class B extends A {
+    @Override
+    public void methodA(int k) {
+        j = k * k;
+        System.out.println("Class B : j = " + j);
+    }
+    public void methodB() {
+        System.out.println("Class B");
+    }
+}
+// Hie-rar-chi-cal (4 syllables) Inheritance
+class C extends A {
+    public void methodA(int x) { // method overriding? YES!
+        j = x * 5;
+        System.out.println("j = " + j);
+    }
+    public void methodC() {
+        System.out.println("Class C");
+    }
+}
+class D extends B {
+    public void methodD(int k) {
+        System.out.println("Class D");
     }
 }
