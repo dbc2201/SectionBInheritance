69a8b97669f1be9bb2a40fbc0a23d7688b4c384d
dbc2201 <divyanshb22@gmail.com>
2020-08-28 12:39:10 +0530

    set the values and display

--- src/definitions/examples/Example5.java
+++ src/definitions/examples/Example5.java
@@ -10,6 +10,14 @@ public class Example5 {
     public static void main(String[] args) {
         A2 objectA2 = new A2();
         B2 objectB2 = new B2();
+        objectA2.setValues(4, 5);
+        objectA2.m = 10;
+        objectA2.displayA();
+        objectB2.p = 5;
+        objectB2.m = 20;
+        objectB2.displayB();
+        System.out.println("objectB2.j = " + objectB2.getJ());
+        System.out.println("objectB2.k = " + objectB2.getK());
     }
 }
 class A2 {
