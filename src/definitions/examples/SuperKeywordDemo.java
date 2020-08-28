e79f0ad209ac5c662070b286120cd9a4a8df5b76
dbc2201 <divyanshb22@gmail.com>
2020-08-28 12:39:41 +0530

    create SuperKeywordDemo.java

---
+++ src/definitions/examples/SuperKeywordDemo.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 28/08/20
+ *  Time: 12:39 PM
+ *  File Name : SuperKeywordDemo.java
+ * */
+package definitions.examples;
+
+public class SuperKeywordDemo {
+}
