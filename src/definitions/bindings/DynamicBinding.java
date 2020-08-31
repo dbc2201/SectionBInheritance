f91fac28259700707af7328bde32813ad029b68f
dbc2201 <divyanshb22@gmail.com>
2020-08-31 13:19:48 +0530

    create DynamicBinding.java

---
+++ src/definitions/bindings/DynamicBinding.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 31/08/20
+ *  Time: 1:19 PM
+ *  File Name : DynamicBinding.java
+ * */
+package definitions.bindings;
+
+public class DynamicBinding {
+}
