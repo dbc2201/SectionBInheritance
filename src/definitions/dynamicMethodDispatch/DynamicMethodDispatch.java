ea399cd9473fd36573701cb87dc910c588cd3b53
dbc2201 <divyanshb22@gmail.com>
2020-09-02 13:25:08 +0530

    add example of the dynamic method dispatch;eod

--- src/definitions/dynamicMethodDispatch/DynamicMethodDispatch.java
+++ src/definitions/dynamicMethodDispatch/DynamicMethodDispatch.java
@@ -15,6 +15,24 @@ public class DynamicMethodDispatch {
         RacingBike racingBike = new RacingBike();
         racingBike.display();
         // which method would be called? #1, #2 or #3??
+        DeliveryBike deliveryBike = new DeliveryBike();
+        deliveryBike.display();
+        Bike newBike = deliveryBike;
+        // so, we have shared the reference value of `bike` to `newBike`.
+        // both the variables refer to the same object.
+        newBike.display();
+        // which method would be called? #1, #2 or #3??
+        // even if the reference variable is created with the Parent class type,
+        // the actual reference value is of the child class type,
+        // and since, we have overridden the display()  method in the child class as well
+        // we can still call it.
+        // This is known as Dynamic Method Dispatch.
+        // Calling the overridden version of a method from the child class,
+        // with the reference variable of the parent class.
+        // It only works if the child class has overridden the method from the
+        // parent class.
+        // If the child class does not have an overridden method, the method
+        // from the parent class will be called.
     }
 }
 
@@ -32,8 +50,12 @@ class RacingBike extends Bike {
 }
 
 class DeliveryBike extends Bike {
-    @Override
-    public void display() { // 3
-        System.out.println("DeliveryBike Class.");
-    }
+//    @Override
+//    public void display() { // 3
+//        System.out.println("DeliveryBike Class.");
+//    }
+    // if we comment out the above code,
+    // then the DeliveryBike class does not have any overridden version of
+    // the display() method, in this case, the display method from the
+    // Bike class will be called.
 }
