5b6d1fa99eb693bd9a8068d843abc7b0a90d54ff
dbc2201 <divyanshb22@gmail.com>
2020-09-02 13:12:39 +0530

    override the display() method in the RacingBike class

--- src/definitions/dynamicMethodDispatch/DynamicMethodDispatch.java
+++ src/definitions/dynamicMethodDispatch/DynamicMethodDispatch.java
@@ -19,7 +19,10 @@ class Bike {
 }
 
 class RacingBike extends Bike {
-
+    @Override
+    public void display() {
+        System.out.println("RacingBike Class.");
+    }
 }
 
 class DeliveryBike extends Bike {
