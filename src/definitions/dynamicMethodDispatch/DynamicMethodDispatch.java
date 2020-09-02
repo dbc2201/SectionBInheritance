9e8243288499aa12a78c9c1ce26c329e31d1ffe9
dbc2201 <divyanshb22@gmail.com>
2020-09-02 13:16:54 +0530

    create an object of the RacingBike class

--- src/definitions/dynamicMethodDispatch/DynamicMethodDispatch.java
+++ src/definitions/dynamicMethodDispatch/DynamicMethodDispatch.java
@@ -9,25 +9,31 @@ package definitions.dynamicMethodDispatch;
 public class DynamicMethodDispatch {
     public static void main(String[] args) {
         Bike bike = new Bike();
+        bike.display();
+        // what would be the output?
+        // which method would be called? #1, #2 or #3??
+        RacingBike racingBike = new RacingBike();
+        racingBike.display();
+        // which method would be called? #1, #2 or #3??
     }
 }
 
 class Bike {
-    public void display() {
+    public void display() { // 1
         System.out.println("Bike Class.");
     }
 }
 
 class RacingBike extends Bike {
     @Override
-    public void display() {
+    public void display() { // 2
         System.out.println("RacingBike Class.");
     }
 }
 
 class DeliveryBike extends Bike {
     @Override
-    public void display() {
+    public void display() { // 3
         System.out.println("DeliveryBike Class.");
     }
 }
