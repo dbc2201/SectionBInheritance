f8cefc2d6450b9a90ef670a92bdaeca7a6b2a726
dbc2201 <divyanshb22@gmail.com>
2020-09-03 13:30:50 +0530

    create Interface PI in PI.java

---
+++ src/definitions/interfaces/PI.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 03/09/20
+ *  Time: 1:30 PM
+ *  File Name : PI.java
+ * */
+package definitions.interfaces;
+
+public interface PI {
+}
