27cf18e776d75ef9df10a18b2588408e0600ab43
dbc2201 <divyanshb22@gmail.com>
2020-09-03 13:32:33 +0530

    create Circle.java

---
+++ src/definitions/interfaces/Circle.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 03/09/20
+ *  Time: 1:32 PM
+ *  File Name : Circle.java
+ * */
+package definitions.interfaces;
+
+public class Circle {
+}
