05ab6d3ef2ff9088e2d3b62ff9695d1f90a5c05e
dbc2201 <divyanshb22@gmail.com>
2020-09-03 13:29:59 +0530

    create Interface Radius in Radius.java

---
+++ src/definitions/interfaces/Radius.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 03/09/20
+ *  Time: 1:29 PM
+ *  File Name : Radius.java
+ * */
+package definitions.interfaces;
+
+public interface Radius {
+}
