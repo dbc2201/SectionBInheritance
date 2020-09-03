cab825aa91873e81275c875ade91d924ee015f6c
dbc2201 <divyanshb22@gmail.com>
2020-09-03 13:31:48 +0530

    create an interface AreaCircle in AreaCircle.java

---
+++ src/definitions/interfaces/AreaCircle.java
@@ -0,0 +1,11 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 03/09/20
+ *  Time: 1:31 PM
+ *  File Name : AreaCircle.java
+ * */
+package definitions.interfaces;
+
+public interface AreaCircle {
+
+}
