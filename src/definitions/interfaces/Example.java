9ec995ece77266e7b720aa51c74eb36c70bc09bf
dbc2201 <divyanshb22@gmail.com>
2020-09-03 13:20:33 +0530

    create interfaces.Example.java

---
+++ src/definitions/interfaces/Example.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 03/09/20
+ *  Time: 1:20 PM
+ *  File Name : Example.java
+ * */
+package definitions.interfaces;
+
+public class Example {
+}
