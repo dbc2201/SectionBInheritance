88692486a99a1de648c593e93c4d3eb7eebaa4da
dbc2201 <divyanshb22@gmail.com>
2020-09-04 12:33:57 +0530

    eod

--- src/bank/IOBBank.java
+++ src/bank/IOBBank.java
@@ -6,5 +6,23 @@
  * */
 package bank;
 
-public class IOBBank {
+public class IOBBank implements Bank, ATM{
+
+    @Override
+    public void withdrawMoney() {
+        dispenseMoney(1000.0);
+    }
+
+    private void dispenseMoney(double amount) {
+
+    }
+
+    @Override
+    public void createBankAccount() {
+        fillAccountOpeningForm();
+    }
+
+    private void fillAccountOpeningForm() {
+
+    }
 }
